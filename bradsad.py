0178b05e23b834af795c50cb7c09a378339a0e25
outsider1113 <56751350+outsider1113@users.noreply.github.com>
2021-01-02 15:06:35 -0800

    Update bradsad.py

--- bradsad.py
+++ bradsad.py
@@ -1,11 +1,13 @@
 import time, discord, requests
+import oauth2 as oauth, urllib
 
 path = "https://api.schoology.com/v1"
 key = "6c457bdf6661e60b42292540a754394e05faf105c"
 secret = "7595214e6c1a35452960e2fbfe0bafe9"
-schoololooooogy = requests.get("https://api.schoology.com/v1", auth=(secret, key))
-print(schoololooooogy)
-messageName = 0
+#schoololooooogy = requests.get("https://api.schoology.com/v1")
+
+#print(schoololooooogy)
+
 class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
@@ -25,3 +27,21 @@ if (sadbrad):
     print("brad is sad")
 else:
     print("brad is glad")
+
+
+def oauth_req(url, key, secret, http_method="POST", post_body=None, http_headers=None):
+    CONSUMER_KEY = key
+    CONSUMER_SECRET = secret
+    consumer = oauth.Consumer(key=CONSUMER_KEY, secret=CONSUMER_SECRET)
+    token = oauth.Token(key=key, secret=secret)
+    client = oauth.Client(consumer, token)
+    content = client.request(
+        url,
+        method=http_method,
+        body= urllib.urlencode({'status': post_body}),
+        headers=http_headers,
+        force_auth_header=True,
+    )
+    return content
+
+print(oauth_req(path, key, secret))
