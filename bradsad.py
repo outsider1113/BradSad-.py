226039f8f0ba777e9feb99761a856a279245bc94
BrandoWithTheLambo <76855497+BrandoWithTheLambo@users.noreply.github.com>
2021-01-02 13:57:49 -0800

    removed pyautogui temporarly and also revolutionized computer science as a whole

--- bradsad.py
+++ bradsad.py
@@ -1,11 +1,15 @@
-import pyautogui, time, discord
+import time, discord
 
+messageName = 0
 class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
 
     async def on_message(self, message):
-        print(self)
+        print(message.author,":", message.content)
+        print(message.author.id)
+        if (message.author.id == 343403664175792128):
+            await message.channel.send("{0.author.mention} stop being sad, brad".format(message))
 
 client = MyClient()
 client.run("Nzk0ODA5MzYzOTczMjc1NjQ4.X_AN5w.cZvphqU4FLUrG4Kl4H7p-29l1uE")
