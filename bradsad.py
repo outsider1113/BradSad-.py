f55882232b1a60e34b107bfa60d4c33d70baecd5
outsider1113 <56751350+outsider1113@users.noreply.github.com>
2021-01-01 22:15:31 -0800

    Update bradsad.py

--- bradsad.py
+++ bradsad.py
@@ -5,10 +5,10 @@ class MyClient(discord.Client):
         print('Logged on as {0}!'.format(self.user))
 
     async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
+        print(self)
 
 client = MyClient()
-client.run(process.token.user)
+client.run("Nzk0ODA5MzYzOTczMjc1NjQ4.X_AN5w.cZvphqU4FLUrG4Kl4H7p-29l1uE")
 
 print("Sad Brad")
 sadbrad = True 
