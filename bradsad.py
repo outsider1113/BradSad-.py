c421220c31262af1af6d1e1faddf60e85d6324eb
BrandoWithTheLambo <76855497+BrandoWithTheLambo@users.noreply.github.com>
2021-01-03 16:09:48 -0800

    seperated the schoology class into a new file for easier future endeavors

--- bradsad.py
+++ bradsad.py
@@ -4,31 +4,36 @@ import requests_oauthlib
 import json
 import pytz
 from datetime import datetime
-
-try:
-    from json.decoder import JSONDecodeError
-except ImportError:
-    JSONDecodeError = ValueError
+from getschoologystuff import schoology as schoology
 
 path = "https://api.schoology.com/v1"
 key = "6c457bdf6661e60b42292540a754394e05faf105c"
 secret = "7595214e6c1a35452960e2fbfe0bafe9"
-
+userscode = "55633162"
 class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
 
     async def on_message(self, message):
-        if (message.content == "+"):
+        if (message.content == "+today"):
             print(message.author,":", message.content)
             assignmentDict = sortAssignments(schoology(key,secret))
             #valueList = list(assignmentDict.values())
             try:
-                cdate = getCurrentDate(True)
-                temp = assignmentDict["2020-08-31"]
+                cdate = getCurrentDate(False)
+                temp = assignmentDict[cdate]
                 await message.channel.send(embed = sendEmbed(discord,temp[0],temp[3],cdate,temp[4], temp[2], temp[1]))
             except KeyError:
-                await message.channel.send("There are no assignments due today")    
+                await message.channel.send(message.author.mention + " There are no assignments due today")
+        elif (message.content == "+nextday"):
+            print(message.author,":", message.content)
+            assignmentDict = sortAssignments(schoology(key,secret))
+            try:
+                nextDate = getCurrentDate(True)
+                temp = assignmentDict[nextDate] #replace brackets with nextDate
+                await message.channel.send(embed = sendEmbed(discord,temp[0],temp[3],nextDate,temp[4], temp[2], temp[1]))
+            except KeyError:
+                await message.channel.send(message.author.mention +  " There are no assignments due Tomorrow")
 
 def sendEmbed(disc, title, desc, date, typeof, points, time):
     #Creates embed and returns it 
@@ -76,41 +81,9 @@ def convertTime(milTime):
     standardTime = str(temptime[0]) + ':' + temptime[1] + ' '+ temptime[2]
     return standardTime
 
-class schoology:
-    def __init__(self, consumer_key, consumer_secret, domain='https://www.schoology.com', three_legged=False,
-                    request_token=None, request_token_secret=None, access_token=None, access_token_secret=None):
-            #establishes the root and domain which we already know 
-            self.API_ROOT = 'https://api.schoology.com/v1'
-            self.DOMAIN_ROOT = domain
-            
-            #allows to establish the key and secret we give it     
-            self.consumer_key = consumer_key
-            self.consumer_secret = consumer_secret
-            
-            #tokens for 3 legged oauth
-            self.request_token = request_token
-            self.request_token_secret = request_token_secret
-            self.access_token = access_token
-            self.access_token_secret = access_token_secret
-
-            #requests self user with oauth using given secret and key 
-            self.oauth = requests_oauthlib.OAuth1Session(self.consumer_key, self.consumer_secret)
-            self.three_legged = three_legged
-
-    def getuserinfo(self):
-        try:
-            user = self.oauth.get("https://api.schoology.com/v1/users/55633162")
-            return user.json()
-        except JSONDecodeError:
-            return{}
-
-    def getcourses(self, start, limit):
-        try:
-            getlink = "https://api.schoology.com/v1/sections/2535704616/assignments"+"?start="+str(start)+"&limit="+str(limit)
-            courses = self.oauth.get(getlink)
-            return courses.json()
-        except JSONDecodeError:
-            return{}
+sc = schoology(key,secret)
+scgetallcourses = sc.getuserinfo(userscode)
+print(scgetallcourses)
 
 def sortAssignments(school):
     sc = school
---
+++ getschoologystuff.py
@@ -0,0 +1,45 @@
+import time, discord, requests
+import oauth2 as oauth, urllib
+import requests_oauthlib
+import json
+import pytz
+from datetime import datetime
+try:
+    from json.decoder import JSONDecodeError
+except ImportError:
+    JSONDecodeError = ValueError
+class schoology:
+    def __init__(self, consumer_key, consumer_secret, domain='https://www.schoology.com', three_legged=False,
+                    request_token=None, request_token_secret=None, access_token=None, access_token_secret=None):
+            #establishes the root and domain which we already know 
+            self.API_ROOT = 'https://api.schoology.com/v1'
+            self.DOMAIN_ROOT = domain
+            
+            #allows to establish the key and secret we give it     
+            self.consumer_key = consumer_key
+            self.consumer_secret = consumer_secret
+            
+            #tokens for 3 legged oauth
+            self.request_token = request_token
+            self.request_token_secret = request_token_secret
+            self.access_token = access_token
+            self.access_token_secret = access_token_secret
+
+            #requests self user with oauth using given secret and key 
+            self.oauth = requests_oauthlib.OAuth1Session(self.consumer_key, self.consumer_secret)
+            self.three_legged = three_legged
+
+    def getuserinfo(self,usercode):
+        try:
+            user = self.oauth.get("https://api.schoology.com/v1/users/" + usercode + "/sections")
+            return user.json()
+        except JSONDecodeError:
+            return{}
+
+    def getcourses(self, start, limit):
+        try:
+            getlink = "https://api.schoology.com/v1/sections/2535704616/assignments"+"?start="+str(start)+"&limit="+str(limit)
+            courses = self.oauth.get(getlink)
+            return courses.json()
+        except JSONDecodeError:
+            return{}
