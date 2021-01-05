976078bcba7b6bd3ff38d6bcb459acae90ad61d1
BrandoWithTheLambo <76855497+BrandoWithTheLambo@users.noreply.github.com>
2021-01-04 18:00:31 -0800

    we can now communicate with the aliens known as pgAdmin

--- db.py
+++ db.py
@@ -1,6 +1,15 @@
 import psycopg2
 
-DB_HOST = ""
-DB_NAME = ""
-DB_USER = ""
-DB_PASS = ""
+con = psycopg2.connect(
+    host = "localhost",
+    database = "postgres",
+    user = "postgres",
+    password = "ballsdeep69",
+    port = 5432)
+#bot_guilds
+cur = con.cursor()
+cur.execute("select 1 from bot_guilds where guild = '121212212'  ")
+rows = cur.fetchall()
+print (rows)
+cur.close()
+con.close()
